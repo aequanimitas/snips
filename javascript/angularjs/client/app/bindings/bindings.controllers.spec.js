8a078a6ccebfb47ea5f9eb65571e481511251ed5
metalfatigue <htavenido@gmail.com>
2015-05-25 18:13:15 +0800

    - added initial set of failing tests - tests should be seperated too by concern

--- javascript/angularjs/client/app/bindings/bindings.controllers.spec.js
+++ javascript/angularjs/client/app/bindings/bindings.controllers.spec.js
@@ -1,6 +1,18 @@
 'use strict';
 
-describe('Controller: BindingsCtrl', function(){
+describe('Submodule: Bindings - ', function(){
 
   beforeEach(module('angularSnippetsApp'));
+
+  describe('Directive', function(){
+    it('should have tests', function(){
+      expect(false).toBe(true);
+    });
+  });
+
+  describe('Controller', function(){
+    it('should have tests', function(){
+      expect(false).toBe(true);
+    });
+  });
 });
