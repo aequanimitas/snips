7a427af7d00db91fcd56b66d5ca9720ada8f280f
aequanimitas <htavenido@gmail.com>
2016-01-02 06:35:46 +0800

    just use []

--- nobulaga.js
+++ nobulaga.js
@@ -3,7 +3,7 @@
   d=document;
   x=d.getElementsByClassName('yt-shelf-grid-item');
   w.addEventListener('scroll', function(){
-    Array.prototype.filter.call(x, function(y){
+    [].filter.call(x, function(y){
       if(y.innerHTML.includes(blah))
         y.style.display='none';
       return cond;
