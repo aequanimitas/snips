8a2be4e673b541de5e208dd0026929b43a7d24a5
aequanimitas <htavenido@gmail.com>
2016-03-02 23:39:47 +0800

    lyah more

--- haskell/lyah/0001.hs
+++ haskell/lyah/0001.hs
@@ -8,10 +8,17 @@ doubleYeah x y = doubleMe x + doubleMe y
 
 
 -- lists
-longList = [1...1000000]
+longList = [1..1000000]
 
 charA = 'A'
 charBrackets = "A"
 
 -- charBrackets:" LONG" wont work
 -- charA:" LONG" will
+--
+-- ranges
+reverseCoundown = [20,19..1]
+takeMultiples = take 10000 [1,1000..]
+
+repeatTake = take 10 (repeat 5)
+replicate3 x y = replicate x y
