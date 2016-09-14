8da33a8a9d10bd925d2692e68f77ab59064dac48
aequanimitas <htavenido@gmail.com>
2016-09-14 11:16:10 +0800

    explaination on second test

--- js/types/obj.js
+++ js/types/obj.js
@@ -9,10 +9,10 @@ function reassignProp(obj) {
 }
 
 test('Mutations in JS', function(t) {
-  var obj = { x: 1};
-  t.plan(1);
+  var obj = { x: 1}, obj2 = { x: 1 };
+  t.plan(2);
   reassignArg(obj);
   t.equal(obj.x, 1, 'JS is Call by Value, arguments are just copies and only exists until the end of the calling environment');
-  reassignProp(obj);
-  t.notEqual(obj.x, 2, 'But why this happens?');
+  reassignProp(obj2);
+  t.notEqual(obj.x, 2, 'JS is also call by sharing! The modification made in the called fn is also visible to the caller since they share the same object, if an assignment is made to the argument, there is no effect on the caller');
 })
