686b19b754c002d87059a4c739adb2b5fa29ac84
aequanimitas <htavenido@gmail.com>
2016-09-24 00:09:01 +0800

    deep diving timers

--- js/event-loop/event-loop.js
+++ js/event-loop/event-loop.js
@@ -1,15 +1,49 @@
-var stack = 0;
-var hi = {
-  toString: function() {
-    return 'Hi';
+var fs = require('fs');
+
+process.nextTick(function() {
+  console.log('inside nextTick');
+});
+
+setImmediate(function() {
+  console.log('CHECK phase: inside setImmediate, delay argument empty and will be called after setTimeout');
+});
+
+setTimeout(function() {
+  console.log('TIMER phase: inside setTimeout, delay argument empty.');
+});
+
+setTimeout(function() {
+  console.log('TIMER phase: inside setTimeout, testing same arg 0001.');
+}, 2);
+
+setTimeout(function() {
+  console.log('TIMER phase: inside setTimeout, testing same arg 0000.');
+}, 1);
+
+process.nextTick(function() {
+  console.log('inside nextTick, after setTimeouts are called and their callbacks are placed in the stack queue');
+});
+
+var x = function(cb) {
+  var b = 0;
+  while(b !== 1000000) {
+    b++;
+    if (b === 1000000) {
+      process.nextTick(cb);
+    }
   }
 }
 
-console.trace(hi);
+setTimeout(function() {
+  console.log('this callback function is pushed immediately at the end queue / task queue');
+}, 2147483648);
 
 setTimeout(function() {
-  console.trace('event loop will pass this to the stack only when the stack is clear');
-  console.trace('hello');
-}, 2000);
+  console.log('TIMER phase: inside setTimeout, negative int argument.');
+}, -1);
 
-console.trace('nice');
+console.log('normal function call');
+
+x(function() {
+  console.log('done');
+});
