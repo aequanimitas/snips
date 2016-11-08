a95348cf695c4904a28aa256b0330097f27c17c3
aequanimitas <htavenido@gmail.com>
2016-11-08 23:33:25 +0800

    notes

---
+++ elixir/mit-algo/001-peak-finding.ex
@@ -0,0 +1,7 @@
+defmodule PeakFinding do
+  def sforward(lst) do
+
+  end
+end
+
+ExUnit.start
---
+++ elixir/mit-algo/notes-ch-001.ex
@@ -0,0 +1,2 @@
+# Runtime of insertion sort
+IO.puts div(trunc(2.0e14), trunc(1.0e10)) / :math.pow(60, 2)
