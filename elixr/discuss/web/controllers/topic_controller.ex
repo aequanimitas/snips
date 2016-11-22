8dec007c5538e8d2032d078b4994280349c258d9
aequanimitas <htavenido@gmail.com>
2016-11-22 08:52:19 +0800

    function plugs for auth

--- elixr/discuss/web/controllers/topic_controller.ex
+++ elixr/discuss/web/controllers/topic_controller.ex
@@ -4,6 +4,7 @@ defmodule Discuss.TopicController do
   alias Discuss.Topic
 
   plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:update, :edit, :delete]
 
   def index(conn, _params) do
     IO.inspect(conn.assigns)
@@ -13,7 +14,11 @@ defmodule Discuss.TopicController do
 
   def create(conn, %{"topic" => topic}) do
     # pass empty struct
-    changeset = Topic.changeset(%Topic{}, topic)
+    # conn.assigns[:user] or conn.assigns.user
+    changeset = conn.assigns.user
+      |> build_assoc(:topics)
+      |>Topic.changeset(topic)
+
     case Repo.insert(changeset) do
       {:ok, _topic} -> 
         conn 
@@ -61,5 +66,20 @@ defmodule Discuss.TopicController do
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
 
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+    user = Repo.get(Topic, topic_id)
+    if user do
+      if user.user_id == conn.assigns.user.id do
+        conn
+      end
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
   end
 end
