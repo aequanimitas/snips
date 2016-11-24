1369656e4dc02f20bf5f55dda91fbb1198b2d423
aequanimitas <htavenido@gmail.com>
2016-11-24 14:13:28 +0800

    this is just amazing, example from SO, adjusted

---
+++ elixr/otp/lib/say_hi.ex
@@ -0,0 +1,22 @@
+defmodule Otp.SayHi do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  def init(state) do
+    schedule_work()
+    {:ok, state}
+  end
+
+  def handle_info(:work, state) do
+    IO.puts "HI"
+    schedule_work()
+    {:noreply, state}
+  end
+
+  defp schedule_work() do
+    Process.send_after(self(), :work, 1000)
+  end
+end
