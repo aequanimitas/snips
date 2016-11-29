0e45c67a4f1b81c36374dd4e9e7e0590ca5c144f
aequanimitas <htavenido@gmail.com>
2016-11-29 22:05:56 +0800

    transform response to GameDay struct

--- elixr/lib/nba.ex
+++ elixr/lib/nba.ex
@@ -1,24 +1,13 @@
 defmodule Elixr.Nba do
 
   defmodule GameDay do
-    defstruct [:games_list]
-  end
-
-  defmodule Game do
-    defstruct [
-      :game_date, :game_sequence, :game_id, :game_status_id, :game_status,
-      :game_code, :hometeam_id, :visitor_team_id, :season, :live_period,
-      :live_pctime, :broadcaster_abbr, :live_bcast, :wh_status
-    ]
-    @type t :: %__MODULE__{game_date: String.t}
+    defstruct [:game_header, :line_score, :series_standings, :last_meeting, :data]
   end
 
   def init() do
     scores(Timex.today)
     |> parse_results
-    |> JSON.decode!
-    |> games_today
-    |> show_games_list
+    |> get_game_header
   end
 
   def scores(%Date{month: m, year: y, day: d} = _dte) do
@@ -27,63 +16,23 @@ defmodule Elixr.Nba do
 
   defp parse_results(result) do
     case result do
-      {:ok, %HTTPoison.Response{body: body}} -> body
+      {:ok, %HTTPoison.Response{body: data}} -> 
+        %GameDay{data: JSON.decode!(data)}
       _ -> "Fail miserably"
     end
   end
 
-  @doc """
-
-  """
-  def games_today(json) do
-    r = hd json["resultSets"]
-    games = for row <- r["rowSet"] do
-      for {k,v} <- Enum.zip(r["headers"], row) do
-        %{"#{k}" => "#{v}"}
+  def to_map(result_set) do
+    rset = for row <- result_set["rowSet"] do
+      for {k,v} <- Enum.zip(result_set["headers"], row) do
+        {"#{k}", "#{v}"}
       end
     end
-    for game <- games do
-      to_game game
-    end
+    Enum.map(rset, fn(rrow) -> Enum.into rrow, %{} end)
   end
 
-  def to_game([
-    %{"GAME_DATE_EST" => game_date},
-    %{"GAME_SEQUENCE" => game_sequence},
-    %{"GAME_ID" => game_id},
-    %{"GAME_STATUS_ID" => game_status_id},
-    %{"GAME_STATUS_TEXT" => game_status},
-    %{"GAMECODE" => game_code},
-    %{"HOME_TEAM_ID" => hometeam_id},
-    %{"VISITOR_TEAM_ID" => visitorteam_id},
-    %{"SEASON" => season},
-    %{"LIVE_PERIOD" => live_period},
-    %{"LIVE_PC_TIME" => live_pctime},
-    %{"NATL_TV_BROADCASTER_ABBREVIATION" => broadcaster_abbr},
-    %{"LIVE_PERIOD_TIME_BCAST" => live_bcast},
-    %{"WH_STATUS" => wh_status},
-  ]) do
-    %Game{
-      game_status_id: game_status_id, 
-      game_date: game_date,
-      game_sequence: game_sequence,
-      game_id: game_id, 
-      game_status: game_status,
-      game_code: game_code, 
-      hometeam_id: hometeam_id,
-      visitor_team_id: visitorteam_id, 
-      season: season, 
-      live_period: live_period,
-      live_pctime: live_pctime, 
-      broadcaster_abbr: broadcaster_abbr, 
-      live_bcast: live_bcast, 
-      wh_status: wh_status
-    }
+  def get_game_header(%GameDay{data: data} = resultSets) do
+    game_header = to_map(hd data["resultSets"])
+    %GameDay{resultSets | game_header: game_header}
   end
-
-  def show_games_list(games) do
-    Enum.each games, fn(game) -> 
-      IO.puts " #{String.replace(game.game_code, "/", " ") }"
-    end
-  end 
 end
