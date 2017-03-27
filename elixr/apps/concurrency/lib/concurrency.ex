f0a219db34e918b16396d95a3c9c6f13c3ffb799
aequanimitas <htavenido@gmail.com>
2017-03-27 10:42:23 +0800

    commit everything before transferring notes here

---
+++ elixr/apps/concurrency/lib/concurrency.ex
@@ -0,0 +1,43 @@
+defmodule Concurrency do
+  @moduledoc """
+  Documentation for Concurrency.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Concurrency.hello
+      :world
+
+  """
+  def start(count) do
+    pid0 = spawn(__MODULE__, :bar, [])
+    spawn(__MODULE__, :foo, [pid0, count])
+  end
+
+  def foo(_pid, 0) do
+    IO.puts "DONE"
+  end
+
+  def foo(pid, count) do
+    send pid, {:foo, self()}
+    receive do
+      {:bar, _pid} ->
+        IO.puts "Conversations remaining: #{count}"
+        # recusion, tail-call optimization means the last computation that needs to happen
+        # is the recursive call
+        foo(pid, count - 1)  
+    end
+  end
+
+  def bar do
+    receive do
+      {:foo, pid} ->
+        IO.puts "foo received, sending back"
+        send pid, {:bar, self()}
+        bar()
+    end
+  end
+end
--- elixr/apps/macros/lib/macros_math.exs
+++ elixr/apps/macros/lib/macros_math.exs
@@ -2,6 +2,7 @@ defmodule Macros.Math do
 
   # macros receive the AST representation
   # so 1 + 1 is converted to AST: {:+, [context: Elixir, import: Kernel], [1,2]}
+  # by pattern matching
   defmacro say(args = {:+, _, [lhs, rhs]}) do
     quote do
       # all labels that were used in desctructuring seems "unavailable" until doing
