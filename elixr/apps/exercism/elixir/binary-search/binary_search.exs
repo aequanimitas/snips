03cd50b89005ef5bc96aa85ec4636f7d7bf92b83
aequanimitas <htavenido@gmail.com>
2017-05-05 12:35:26 +0800

    working binary search

--- elixr/apps/exercism/elixir/binary-search/binary_search.exs
+++ elixr/apps/exercism/elixir/binary-search/binary_search.exs
@@ -1,5 +1,6 @@
 defmodule BinarySearch do
   require Logger
+  require Integer
   @doc """
     Searches for a key in the tuple using the binary search algorithm.
     It returns :not_found if the key is not in the tuple.
@@ -21,67 +22,31 @@ defmodule BinarySearch do
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
   def search(numbers, key) do
     lst = Tuple.to_list(numbers)
-    idx = 0
-    cond do
-      length(lst) == 0 -> :not_found
-      length(lst) == 1 -> 
-        if hd(lst) == key do
-          {:ok, idx}
-        else
-          :not_found
+    search(lst, key, 0)
+  end
+
+  def search(numbers, key, index) do
+    len = length(numbers)
+    idx = div(len, 2)
+    case length(numbers) do
+      0 -> :not_found
+      1 -> if key == hd(numbers), do: {:ok, index + idx}, else: :not_found 
+      _ ->
+        {left, right} = Enum.split(numbers, idx)
+        h_left = left |> Enum.reverse |> hd
+        h_right = hd(right)
+        cond do
+          key == h_right -> {:ok, idx + index}
+          key > h_left -> search(right, key, index + idx)
+          key == h_left -> 
+            idx = case Integer.is_even(idx) do
+                    true -> index + idx - 1
+                    false -> index + idx
+                 end
+            {:ok, idx}
+          key < h_left -> search(left, key, index - 1)
+          true -> :not_found
         end
-      true -> search(lst, key, idx)
-    end
-  end
-
-  def search(lst, key, index) do
-    {lst, idx} = chunk(lst)
-    if length(lst) == 1 do
-      [[h]] = lst
-      if key == h do
-        {:ok, idx}
-      else
-        :not_found
-      end
-    else
-      [l_side, r_side] = lst
-      l_head = 
-        l_side
-        |> Enum.reverse
-        |> hd
-      r_head = hd(r_side)
-      cond do
-        key > r_head -> search(r_side, key, idx + index)
-        key < l_head -> search(r_side, key, idx - index)
-        true -> {:ok, idx - 1}
-      end
-    end
-    # conditions to be met
-    # if lst length is 1
-    #   destructure lst as [[h]]
-    #   if key is equal to h
-    #     return {:ok, index}
-    #   else
-    #     return :not_found
-    # else 
-    #   destructure lst into l_side and r_side
-    #   reverse l_side and get its head, assign to left
-    #   get r_side head, assign to right
-    #  
-  end
-
-  @doc """
-  returns the chunks of the list along with length of the list in half
-  the length of the half will either be:
-  - added to the index tracker if its greater than the `left` side's last elem
-  - or subtracted to the index tracker if its less than the `left` side's last elem
-  """
-  def chunk(lst) do
-    len = length(lst)
-    step = div(len, 2)
-    case rem(len, 2) == 0 do
-      true -> {Enum.chunk(lst, step, step, []), step}
-      false -> {Enum.chunk(lst, step + 1, step + 1, []), step + 1}
     end
   end
 end
--- elixr/apps/exercism/elixir/binary-search/binary_search_test.exs
+++ elixr/apps/exercism/elixir/binary-search/binary_search_test.exs
@@ -12,33 +12,6 @@ defmodule BinarySearchTest do
     assert BinarySearch.search({}, 2) == :not_found
   end
 
-  test "returns {:ok, position} on single elem tuple" do
-    assert BinarySearch.search({1}, 1) == {:ok, 0}
-  end
-
-  test "returns :not_found on single elem tuple" do
-    assert BinarySearch.search({1}, 2) == :not_found
-  end
-
-  # @tag :pending
-  test "properly chunks even-length tuples" do
-    assert BinarySearch.chunk([1,2,3,4]) == {[[1,2], [3,4]], 2}
-    assert BinarySearch.chunk([1,2]) == {[[1], [2]], 1}
-  end
-
-  # @tag :pending
-  test "properly chunks odd-length tuples" do
-    assert BinarySearch.chunk([1,2,3]) == {[[1,2], [3]], 2}
-    assert BinarySearch.chunk([1,2,3,4,5]) == {[[1,2,3], [4,5]], 3}
-    assert BinarySearch.chunk([1]) == {[[1]], 1}
-    assert BinarySearch.chunk([1]) == {[[1]], 1}
-  end
-
-  # @tag :pending
-  test "search/3" do
-    assert BinarySearch.search([2, 4, 6], 3, 0) == :not_found
-  end
-
   # @tag :pending
   test "returns :not_found when key is not in the tuple" do
     assert BinarySearch.search({2, 4, 6}, 3) == :not_found
@@ -64,12 +37,12 @@ defmodule BinarySearchTest do
     assert BinarySearch.search({1, 2, 4, 5, 6}, 4) == {:ok, 2}
   end
 
-  @tag :pending
+  # @tag :pending
   test "finds key when it is the last element in tuple" do
     assert BinarySearch.search({1, 2, 4, 5, 6}, 6) == {:ok, 4}
   end
 
-  @tag :pending
+  # @tag :pending
   test "finds key in a tuple with an even number of elements" do
     tuple = {1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377}
     assert BinarySearch.search(tuple, 21) == {:ok, 5}
